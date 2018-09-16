381b7ca9eb5e2df75ee369610425bfa460deca4b
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-16 19:15:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << artist
+    @@artists << artist unless @@artists.include?(artist)
     @@genres << genre
   end
 
